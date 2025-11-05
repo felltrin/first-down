920bf8be6016f7065d6c1fd7c4c01167019af893
Lucius Holzinger <luciholzinger@gmail.com>
2025-11-05 11:02:20 -0800

    Fix typecheck

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
+        // @ts-ignore
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
